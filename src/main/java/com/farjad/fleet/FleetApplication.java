016787372f6d64c5b56291d34dfd9b4bcdf3bb20
hamid <sahlolbey@gmail.com>
2020-09-18 02:59:08 +0430

    initially added

--- src/main/java/com/farjad/fleet/FleetApplication.java
+++ src/main/java/com/farjad/fleet/FleetApplication.java
@@ -2,6 +2,8 @@ package com.farjad.fleet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @SpringBootApplication
 public class FleetApplication {
@@ -9,5 +11,6 @@ public class FleetApplication {
     public static void main(String[] args) {
         SpringApplication.run (FleetApplication.class , args);
     }
+
 
 }
