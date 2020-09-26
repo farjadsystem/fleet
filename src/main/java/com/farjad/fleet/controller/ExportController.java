581eef72f166e6a776e054f94e9575e82b1e3d10
hamid <sahlolbey@gmail.com>
2020-09-26 15:53:16 +0330

    initially added

--- src/main/java/com/farjad/fleet/controller/ExportController.java
+++ src/main/java/com/farjad/fleet/controller/ExportController.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
@@ -17,12 +18,14 @@ public class ExportController {
     FleetExport fleetExport;
 
     @RequestMapping(value = "download/fleet.xlsx")
-    public void fleetExport(HttpServletResponse response)throws IOException {
+    public void fleetExport(HttpServletResponse response,
+                            @RequestParam String accountID)throws IOException {
         System.out.println ("fleet export called");
         response.setContentType("application/octet-stream");
-        response.setHeader("Content-Disposition", "attachment; filename=customers.xlsx");
+        response.setHeader("Content-Disposition", "attachment; " +
+                "filename=fleets.xlsx");
         ByteArrayInputStream stream =
-                fleetExport.vehicleListToExcelFile (new Locale ("fa"));
+                fleetExport.vehicleListToExcelFile (accountID,new Locale ("fa"));
         IOUtils.copy(stream, response.getOutputStream());
     }
     @RequestMapping(value = "fleet")
