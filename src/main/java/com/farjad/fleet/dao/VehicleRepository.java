c1796ab9b538848b1b8ba1e82f80841e68c505c8
hamid <sahlolbey@gmail.com>
2020-09-26 15:52:36 +0330

    initially added

--- src/main/java/com/farjad/fleet/dao/VehicleRepository.java
+++ src/main/java/com/farjad/fleet/dao/VehicleRepository.java
@@ -1,10 +1,16 @@
 package com.farjad.fleet.dao;
 
 import com.farjad.fleet.model.Vehicle;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface VehicleRepository extends PagingAndSortingRepository<Vehicle,
         Long> {
+    @Query("select v from Vehicle v join fetch v.device where v.account" +
+            ".accountID=?1")
+    List<Vehicle> findByAccount(String accountID);
 }
