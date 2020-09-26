8a68e1f93c1cb3fd64952c5465b26c3a1a7720de
hamid <sahlolbey@gmail.com>
2020-09-26 15:52:56 +0330

    initially added

---
+++ src/main/java/com/farjad/fleet/util/TextUtil.java
@@ -0,0 +1,10 @@
+package com.farjad.fleet.util;
+
+public class TextUtil {
+    public static String nullToStr(Integer i){
+        return i!=null?i.toString ():"";
+    }
+    public static String nullToStr(Double i){
+        return i!=null?i.toString ():"";
+    }
+}
